50ce260698cd0a96b25d9203af2784c5da20587a
Wighawag <wighawag@gmail.com>
2020-03-26 09:08:32 +0000

    do not use blockHandler

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,10 +1,9 @@
 import { store, Address, Bytes, EthereumValue, BigInt } from '@graphprotocol/graph-ts';
-import { Transfer, EIP721 } from '../generated/EIP721/EIP721';
+import { Transfer, LandContract, Minter } from '../generated/Land/LandContract';
 import { LandSaleContract, ReferralUsed, LandQuadPurchased } from '../generated/LandSale/LandSaleContract';
 import { Land, LandReferral, LandPurchase } from '../generated/schema';
 
 import { LandSale } from '../generated/templates'
-import { EthereumBlock } from '@graphprotocol/graph-ts'
 
 import { log } from '@graphprotocol/graph-ts';
 
@@ -14,8 +13,8 @@ export function handleTransfer(event: Transfer): void {
     // log.info('contract : {}',[event.address.toHexString()]);
     // log.info('tokenID : {}',[event.params.id.toHex()]);
     // log.info('new owner : {}',[event.params.to.toHexString()]);
-    let contract = EIP721.bind(event.address);
-    let id = event.params.id.toString();
+    let contract = LandContract.bind(event.address);
+    let id = event.params._tokenId.toString();
     let idAsNumber = i32(parseInt(id, 10));
     let land = Land.load(id);
     if(land == null) {
@@ -23,13 +22,13 @@ export function handleTransfer(event: Transfer): void {
         land.timestamp = event.block.timestamp;
         land.x = idAsNumber % 408;
         land.y = i32(Math.floor(idAsNumber / 408));
-        let metadataURI = contract.try_tokenURI(event.params.id);
+        let metadataURI = contract.try_tokenURI(event.params._tokenId);
         land.tokenURI = metadataURI.value;
-    } else if(event.params.to.toHex() == zeroAddress) { //burnt
+    } else if(event.params._to.toHex() == zeroAddress) { //burnt
         store.remove('Land', id);
     }
-    if(event.params.to.toHex() != zeroAddress) { // ignore transfer to zero
-        land.owner = event.params.to;
+    if(event.params._to.toHex() != zeroAddress) { // ignore transfer to zero
+        land.owner = event.params._to;
         land.save();
     }
 }
@@ -66,11 +65,6 @@ export function handleReferral(event: ReferralUsed): void {
 }
 
 
-export function handleBlock(block: EthereumBlock): void {
-    if (block.number.toString() == "9434370") {
-        LandSale.create(Address.fromString('0xb2fb1d91325d0211b1eb39e4cf2c9f3cf14508b0'));
-    }
-    else if (block.number.toString() == "9048221") {
-        LandSale.create(Address.fromString('0x6ce6267Fc45ABc6051aCee92BF2CC63DcC860A95'));
-    }
+export function handleMinter(event: Minter): void {
+    LandSale.create(event.params.superOperator);
 }
