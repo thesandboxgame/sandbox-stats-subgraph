7197910c0e4fd5dfc5315e77caa56615d08fd6be
Wighawag <wighawag@gmail.com>
2020-03-26 08:38:37 +0000

    use template for multiple land sale

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,6 +1,10 @@
-import { store, Address, Bytes, EthereumValue } from '@graphprotocol/graph-ts';
+import { store, Address, Bytes, EthereumValue, BigInt } from '@graphprotocol/graph-ts';
 import { Transfer, EIP721 } from '../generated/EIP721/EIP721';
-import { Land } from '../generated/schema';
+import { LandSaleContract, ReferralUsed, LandQuadPurchased } from '../generated/LandSale/LandSaleContract';
+import { Land, LandReferral, LandPurchase } from '../generated/schema';
+
+import { LandSale } from '../generated/templates'
+import { EthereumBlock } from '@graphprotocol/graph-ts'
 
 import { log } from '@graphprotocol/graph-ts';
 
@@ -16,7 +20,7 @@ export function handleTransfer(event: Transfer): void {
     let land = Land.load(id);
     if(land == null) {
         land = new Land(id);
-        land.mintTime = event.block.timestamp;
+        land.timestamp = event.block.timestamp;
         land.x = idAsNumber % 408;
         land.y = i32(Math.floor(idAsNumber / 408));
         let metadataURI = contract.try_tokenURI(event.params.id);
@@ -27,5 +31,46 @@ export function handleTransfer(event: Transfer): void {
     if(event.params.to.toHex() != zeroAddress) { // ignore transfer to zero
         land.owner = event.params.to;
         land.save();
+    }
+}
+
+
+export function handlePurchase(event: LandQuadPurchased): void {
+    let id = event.params.topCornerId.toString();
+    let idAsNumber = i32(parseInt(id, 10));
+    let x = idAsNumber % 408;
+    let y = i32(Math.floor(idAsNumber / 408));
+    let purchase = new LandPurchase(id);
+    purchase.topLeftX = x;
+    purchase.topLeftY = y;
+    purchase.buyer = event.params.buyer;
+    purchase.to = event.params.to;
+    purchase.size = event.params.size;
+    purchase.price = event.params.price;
+    purchase.tokenAddress = event.params.token;
+    purchase.amountPaid = event.params.amountPaid;
+    purchase.timestamp = event.block.timestamp;
+    purchase.save();
+}
+
+export function handleReferral(event: ReferralUsed): void {
+    let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+    let referral = new LandReferral(id)
+    referral.referrer = event.params.referrer;
+    referral.referee = event.params.referee;
+    referral.tokenAddress = event.params.token;
+    referral.amount = event.params.amount;
+    referral.commission = event.params.commission;
+    referral.commissionRate = event.params.commissionRate;
+    referral.save();
+}
+
+
+export function handleBlock(block: EthereumBlock): void {
+    if (block.number.toString() == "9434370") {
+        LandSale.create(Address.fromString('0xb2fb1d91325d0211b1eb39e4cf2c9f3cf14508b0'));
+    }
+    else if (block.number.toString() == "9048221") {
+        LandSale.create(Address.fromString('0x6ce6267Fc45ABc6051aCee92BF2CC63DcC860A95'));
     }
 }
